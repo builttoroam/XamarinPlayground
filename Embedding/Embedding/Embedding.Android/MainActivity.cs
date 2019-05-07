79bde9e75b2199b9443dd9c1e311edf591dce6e6
mkieres <mikolaj@builttoroam.com>
2019-05-07 11:20:07 +1000

    Creating Embedding project and adding android specific control

---
+++ Embedding/Embedding/Embedding.Android/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace Embedding.Droid
+{
+    [Activity(Label = "Embedding",
+              Icon = "@mipmap/icon",
+              Theme = "@style/MainTheme",
+              MainLauncher = true,
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : FormsAppCompatActivity
+    {
+        internal static MainActivity Instance { get; private set; }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Instance = this;
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
