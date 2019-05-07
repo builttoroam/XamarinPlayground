a046190ccc70bb842969e5fdc7a8f93e180d1cec
mkieres <mikolaj@builttoroam.com>
2019-05-07 11:20:07 +1000

    Creating Embedding project and adding android specific control

---
+++ Embedding/Embedding/Embedding/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Embedding
+{
+    // NOTE: The XAML compilation needs to be off for pages that use native controls on them, as per documentation https://docs.microsoft.com/en-us/xamarin/xamarin-forms/platform/native-views/xaml
+    // [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
