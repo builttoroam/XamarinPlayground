9f31bd245afa604c75967730911f6d0dafd9e003
mkieres <mikolaj@builttoroam.com>
2019-05-07 15:23:42 +1000

    Creating platform specifics sample

---
+++ P/PlatformSpecifics/PlatformSpecifics/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace PlatformSpecifics
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            On<iOS>().SetUseSafeArea(true);
+        }
+    }
+}
