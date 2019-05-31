32aa1b8b7fa8cf0ff8bbea2e92d36222d98b18db
mkieres <mikolaj@builttoroam.com>
2019-05-31 12:01:19 +1000

    Adding code to demonstrate linking out the code that app is using and solutions to how to presrve that code

---
+++ Linker/Linker/Utilities/StringUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Xamarin.Forms.Internals;
+
+namespace Linker.Utilities
+{
+    // MK If you comment out the Preserve attribute the code that uses reflection to instantiate this class will throw an exception
+    //    as this class and its methods will be stripped out from the assemblies.
+
+    //    NOTE: Provided that the linker.config file is not listing this class as a exception to the rule of stripping out code
+    // [Preserve(AllMembers = true)]
+    public class StringUtilities
+    {
+        public string ReverseString(string textToRevert)
+        {
+            return new string(textToRevert?.Reverse().ToArray());
+        }
+    }
+}
