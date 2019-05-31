cc66394a61b42640cec4eab315d7f5f734d78fe1
mkieres <mikolaj@builttoroam.com>
2019-05-31 12:01:19 +1000

    Adding code to demonstrate linking out the code that app is using and solutions to how to presrve that code

--- Linker/Linker/MainPage.xaml.cs
+++ Linker/Linker/MainPage.xaml.cs
@@ -1,13 +1,26 @@
-﻿using System.ComponentModel;
-using Xamarin.Forms;
+﻿using System;
+using System.Diagnostics;
+using Linker.Utilities;
 
 namespace Linker
 {
     public partial class MainPage
     {
+        private const string EnglishAlphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var stringUtilities = Activator.CreateInstance<StringUtilities>();
+            var reversedAlphabet = stringUtilities.ReverseString(EnglishAlphabet);
+
+            Debug.WriteLine(reversedAlphabet);
         }
     }
 }
